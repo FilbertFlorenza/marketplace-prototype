9985e6c2dc06ce663b49e7726cbdee1b294dc639
Eriec Tanijaya <eriectan88@gmail.com>
2022-07-06 13:20:39 +0700

    test result fix

---
+++ app/Views/parent_dashboard.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Smart Apes</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Mulish&display=swap" rel="stylesheet">
+
+        <script src="https://kit.fontawesome.com/33e5cd664a.js" crossorigin="anonymous"></script>
+
+        <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/styles_test.css');?>">
+    </head>
+
+    <body>
+        <div class="container-md navbar">
+            <img src="/images/parent.png" class="rounded-circle parent-profile-pic" width="50px" height="50px">
+            <div class="welcome-text">Welcome back! 👋</div>
+            <div class="parent-name">Jim Carrey</div>
+
+            
+            <div class="category-text">General</div>
+            <div class="menu-grid-master">
+                <div class="menu-grid text-center">
+                    <a href="#">
+                        <button type="button" class="nav-btn">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M2.5 5.41667C2.5 3.22899 2.52342 2.5 5.41667 2.5C8.30991 2.5 8.33333 3.22899 8.33333 5.41667C8.33333 7.60434 8.34256 8.33333 5.41667 8.33333C2.49077 8.33333 2.5 7.60434 2.5 5.41667Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd" d="M11.6667 5.41667C11.6667 3.22899 11.6902 2.5 14.5834 2.5C17.4767 2.5 17.5001 3.22899 17.5001 5.41667C17.5001 7.60434 17.5093 8.33333 14.5834 8.33333C11.6575 8.33333 11.6667 7.60434 11.6667 5.41667Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M2.5 14.5834C2.5 12.3957 2.52342 11.6667 5.41667 11.6667C8.30991 11.6667 8.33333 12.3957 8.33333 14.5834C8.33333 16.7711 8.34256 17.5001 5.41667 17.5001C2.49077 17.5001 2.5 16.7711 2.5 14.5834Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M11.6667 14.5834C11.6667 12.3957 11.6902 11.6667 14.5834 11.6667C17.4767 11.6667 17.5001 12.3957 17.5001 14.5834C17.5001 16.7711 17.5093 17.5001 14.5834 17.5001C11.6575 17.5001 11.6667 16.7711 11.6667 14.5834Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <div class="nav-text">Dashboard</div>
+                    </a>
+                </div>
+                <div class="menu-grid text-center">
+                    <a href="#">
+                        <button type="button" class="nav-btn">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M13.7615 17.9166H6.80495C4.24965 17.9166 2.28931 16.9937 2.84614 13.279L3.4945 8.24463C3.83775 6.39108 5.02005 5.6817 6.05743 5.6817H14.5395C15.5921 5.6817 16.7058 6.44448 17.1024 8.24463L17.7508 13.279C18.2237 16.5741 16.3168 17.9166 13.7615 17.9166Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path opacity="0.4" d="M13.876 5.49865C13.876 3.51026 12.2641 1.89835 10.2757 1.89835V1.89835C9.31822 1.89429 8.39854 2.27182 7.72005 2.94744C7.04156 3.62307 6.66015 4.54114 6.66016 5.49865H6.66016" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M12.7471 9.25151H12.709" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M7.88825 9.25151H7.85011" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <div class="nav-text">Orders</div>
+                    </a>
+                </div>
+                <div class="menu-grid text-center">
+                    <a href="#">
+                        <button type="button" class="nav-btn">
+                            <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.4" d="M1.57715 7.83688H16.4304" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd" d="M4.54834 11.0914C4.54834 10.6772 4.88413 10.3414 5.29834 10.3414H5.30606C5.72027 10.3414 6.05606 10.6772 6.05606 11.0914C6.05606 11.5056 5.72027 11.8414 5.30606 11.8414H5.29834C4.88413 11.8414 4.54834 11.5056 4.54834 11.0914ZM9.00394 10.3414C8.58973 10.3414 8.25394 10.6772 8.25394 11.0914C8.25394 11.5056 8.58973 11.8414 9.00394 11.8414H9.01166C9.42588 11.8414 9.76166 11.5056 9.76166 11.0914C9.76166 10.6772 9.42588 10.3414 9.01166 10.3414H9.00394ZM12.7018 10.3414C12.2876 10.3414 11.9518 10.6772 11.9518 11.0914C11.9518 11.5056 12.2876 11.8414 12.7018 11.8414H12.7095C13.1238 11.8414 13.4595 11.5056 13.4595 11.0914C13.4595 10.6772 13.1238 10.3414 12.7095 10.3414H12.7018ZM12.7018 13.5802C12.2876 13.5802 11.9518 13.9159 11.9518 14.3302C11.9518 14.7444 12.2876 15.0802 12.7018 15.0802H12.7095C13.1238 15.0802 13.4595 14.7444 13.4595 14.3302C13.4595 13.9159 13.1238 13.5802 12.7095 13.5802H12.7018ZM8.25394 14.3302C8.25394 13.9159 8.58973 13.5802 9.00394 13.5802H9.01166C9.42588 13.5802 9.76166 13.9159 9.76166 14.3302C9.76166 14.7444 9.42588 15.0802 9.01166 15.0802H9.00394C8.58973 15.0802 8.25394 14.7444 8.25394 14.3302ZM5.29834 13.5802C4.88413 13.5802 4.54834 13.9159 4.54834 14.3302C4.54834 14.7444 4.88413 15.0802 5.29834 15.0802H5.30606C5.72027 15.0802 6.05606 14.7444 6.05606 14.3302C6.05606 13.9159 5.72027 13.5802 5.30606 13.5802H5.29834Z" fill="#6D34E0"/>
+                                <path d="M12.3699 1.66669V4.409" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M5.63795 1.66669V4.409" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M12.5319 2.98267H5.4758C3.02856 2.98267 1.5 4.34594 1.5 6.85185V14.3932C1.5 16.9385 3.02856 18.3333 5.4758 18.3333H12.5242C14.9791 18.3333 16.5 16.9622 16.5 14.4563V6.85185C16.5077 4.34594 14.9868 2.98267 12.5319 2.98267Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <div class="nav-text">Calendar</div>
+                    </a>
+                </div>
+                <div class="menu-grid text-center">
+                    <a href="#">
+                        <button type="button" class="nav-btn">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.4" d="M14.9062 9.08057C16.0688 8.91723 16.9638 7.92056 16.9663 6.71306C16.9663 5.52306 16.0988 4.5364 14.9613 4.34973" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M16.4399 11.8752C17.5658 12.0436 18.3516 12.4377 18.3516 13.2502C18.3516 13.8094 17.9816 14.1727 17.3833 14.4011" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M9.90529 12.2198C7.22695 12.2198 4.93945 12.6256 4.93945 14.2465C4.93945 15.8665 7.21279 16.284 9.90529 16.284C12.5836 16.284 14.8703 15.8823 14.8703 14.2606C14.8703 12.639 12.5978 12.2198 9.90529 12.2198Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd" d="M9.90518 9.90656C11.6627 9.90656 13.0877 8.4824 13.0877 6.72406C13.0877 4.96656 11.6627 3.54156 9.90518 3.54156C8.14768 3.54156 6.72268 4.96656 6.72268 6.72406C6.71601 8.47573 8.13018 9.90073 9.88185 9.90656H9.90518Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path opacity="0.4" d="M4.90375 9.08057C3.74042 8.91723 2.84625 7.92056 2.84375 6.71306C2.84375 5.52306 3.71125 4.5364 4.84875 4.34973" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M3.36967 11.8752C2.24384 12.0436 1.45801 12.4377 1.45801 13.2502C1.45801 13.8094 1.82801 14.1727 2.42634 14.4011" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <div class="nav-text">Children</div>
+                    </a>
+                </div>
+                <div class="menu-grid text-center">
+                    <a href="#">
+                        <button type="button" class="nav-btn">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M10.9202 3.48083L12.4431 6.52312C12.5923 6.82179 12.8804 7.02888 13.2145 7.07681L16.6211 7.56717C17.4628 7.68869 17.7977 8.70878 17.1886 9.29327L14.7252 11.6604C14.4831 11.8931 14.3729 12.2277 14.4301 12.5564L15.0115 15.8982C15.1547 16.7249 14.2748 17.3556 13.5225 16.9645L10.4777 15.3856C10.1792 15.2307 9.82168 15.2307 9.52232 15.3856L6.4775 16.9645C5.72519 17.3556 4.84533 16.7249 4.98937 15.8982L5.56987 12.5564C5.62714 12.2277 5.51694 11.8931 5.27485 11.6604L2.8114 9.29327C2.20226 8.70878 2.5372 7.68869 3.37889 7.56717L6.78554 7.07681C7.11961 7.02888 7.40856 6.82179 7.55781 6.52312L9.07979 3.48083C9.45638 2.7286 10.5436 2.7286 10.9202 3.48083Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <div class="nav-text">Favorite</div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="category-text">Account</div>
+            <div class="menu-grid-master">
+                <div class="menu-grid text-center">
+                    <a href="#">
+                        <button type="button" class="nav-btn">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M17.3387 6.35296L16.82 5.45288C16.3812 4.69128 15.4087 4.42855 14.6461 4.86555V4.86555C14.283 5.0794 13.8499 5.14008 13.4421 5.03419C13.0342 4.9283 12.6853 4.66455 12.4722 4.30109C12.3351 4.07007 12.2614 3.80694 12.2586 3.53832V3.53832C12.271 3.10763 12.1085 2.69028 11.8082 2.38134C11.5078 2.0724 11.0952 1.89817 10.6644 1.89835H9.61938C9.19727 1.89834 8.79256 2.06654 8.4948 2.36574C8.19704 2.66493 8.03078 3.07044 8.0328 3.49255V3.49255C8.02029 4.36405 7.3102 5.06396 6.4386 5.06387C6.16997 5.06108 5.90684 4.9874 5.67583 4.85029V4.85029C4.91318 4.41329 3.94072 4.67603 3.50191 5.43763L2.94509 6.35296C2.5068 7.11361 2.76596 8.08545 3.5248 8.52687V8.52687C4.01805 8.81165 4.32191 9.33794 4.32191 9.9075C4.32191 10.4771 4.01805 11.0033 3.5248 11.2881V11.2881C2.76693 11.7266 2.50749 12.6961 2.94509 13.4544V13.4544L3.4714 14.3621C3.677 14.7331 4.02197 15.0069 4.42996 15.1228C4.83795 15.2388 5.27533 15.1874 5.64531 14.98V14.98C6.00903 14.7677 6.44245 14.7096 6.84925 14.8184C7.25604 14.9273 7.60249 15.1942 7.8116 15.5597C7.94871 15.7907 8.02239 16.0538 8.02518 16.3225V16.3225C8.02518 17.2029 8.73893 17.9167 9.61938 17.9167H10.6644C11.5419 17.9167 12.2544 17.2076 12.2586 16.3301V16.3301C12.2565 15.9066 12.4239 15.5 12.7233 15.2006C13.0227 14.9011 13.4294 14.7338 13.8528 14.7359C14.1208 14.743 14.3828 14.8164 14.6156 14.9495V14.9495C15.3762 15.3877 16.3481 15.1286 16.7895 14.3697V14.3697L17.3387 13.4544C17.5513 13.0895 17.6096 12.655 17.5008 12.2469C17.392 11.8389 17.125 11.4911 16.759 11.2805V11.2805C16.3929 11.0699 16.1259 10.7221 16.0171 10.3141C15.9083 9.90604 15.9667 9.47146 16.1793 9.10658C16.3175 8.86523 16.5176 8.66511 16.759 8.52687V8.52687C17.5133 8.08569 17.7718 7.11953 17.3387 6.36059V6.36059V6.35296Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <ellipse opacity="0.4" cx="10.1455" cy="9.90749" rx="2.1968" ry="2.1968" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <div class="nav-text">Settings</div>
+                    </a>
+                </div>
+                <div class="menu-grid text-center">
+                    <a href="#">
+                        <button type="button" class="nav-btn">
+                            <svg width="16" height="20" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.99984 14.873C12.6992 14.873 14.8732 14.2702 15.0832 11.8504C15.0832 9.43232 13.5675 9.58782 13.5675 6.62092C13.5675 4.30344 11.3709 1.66666 7.99984 1.66666C4.62881 1.66666 2.43221 4.30344 2.43221 6.62092C2.43221 9.58782 0.916504 9.43232 0.916504 11.8504C1.1273 14.2793 3.30132 14.873 7.99984 14.873Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path opacity="0.4" d="M9.99039 17.381C8.85361 18.6433 7.08027 18.6582 5.93262 17.381" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <div class="nav-text">Notifications</div>
+                    </a>
+                </div>
+                <div class="menu-grid text-center">
+                    <a href="#">
+                        <button type="button" class="nav-btn">
+                            <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.4" d="M11.0967 12.5195H5.08008" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path opacity="0.4" d="M11.0967 9.03076H5.08008" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path opacity="0.4" d="M7.37591 5.55009H5.08008" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M11.2572 1.2915C11.2572 1.2915 4.85965 1.29484 4.84965 1.29484C2.54965 1.309 1.12549 2.82234 1.12549 5.13067V12.794C1.12549 15.114 2.56049 16.6332 4.88049 16.6332C4.88049 16.6332 11.2772 16.6307 11.288 16.6307C13.588 16.6165 15.013 15.1023 15.013 12.794V5.13067C15.013 2.81067 13.5772 1.2915 11.2572 1.2915Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <div class="nav-text">FAQ</div>
+                    </a>
+                </div>
+                <div class="menu-grid text-center">
+                    <a href="#">
+                        <button type="button" class="nav-btn">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd" d="M13.612 2.29184H6.38783C3.87033 2.29184 2.29199 4.07434 2.29199 6.59684V13.4035C2.29199 15.926 3.86283 17.7085 6.38783 17.7085H13.6112C16.137 17.7085 17.7087 15.926 17.7087 13.4035V6.59684C17.7087 4.07434 16.137 2.29184 13.612 2.29184Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M8.91005 10.0002C8.91005 10.8518 8.21922 11.5435 7.36672 11.5435C6.51505 11.5435 5.82422 10.8518 5.82422 10.0002C5.82422 9.1485 6.51505 8.45683 7.36672 8.45683C8.21922 8.45683 8.91005 9.1485 8.91005 10.0002Z" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M8.91016 10.0002H14.1752V11.5435" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M11.818 11.5431V9.99976" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <div class="nav-text">Password</div>
+                    </a>
+                </div>
+                <div class="menu-grid text-center">
+                    <a href="#">
+                        <button type="button" class="nav-btn">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.4" d="M12.5134 6.15792V5.38042C12.5134 3.68459 11.1384 2.30959 9.44256 2.30959H5.38006C3.68506 2.30959 2.31006 3.68459 2.31006 5.38042V14.6554C2.31006 16.3513 3.68506 17.7263 5.38006 17.7263H9.45089C11.1417 17.7263 12.5134 16.3554 12.5134 14.6646V13.8788" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M18.1748 10.0178H8.14062" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M15.7344 7.58859L18.1744 10.0178L15.7344 12.4478" stroke="#6D34E0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <div class="nav-text">Logout</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+    </body>
+</html>
